c6c959051f4980ac10902058f07d6380dfbb9165
Andrius Andrikonis <andrikonis.andrius@gmail.com>
2023-04-24 17:04:08 +0300

    Intialize dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        'comma-dangle': [
+            'warn',
+            'never'
+        ],
+        'no-unused-vars': 'warn',
+        quotes: [
+            'warn',
+            'single',
+            {
+                avoidEscape: false
+            }
+        ],
+        semi: 'warn',
+        'vue/html-indent': [
+            'warn',
+            4,
+            {
+                baseIndent: 1
+            }
+        ],
+        'vue/html-quotes': [
+            'warn',
+            'double',
+            {
+                avoidEscape: false
+            }
+        ],
+        'vue/multi-word-component-names': 'warn',
+        'vue/no-unused-components': 'warn',
+        'vue/script-indent': [
+            'warn',
+            4,
+            {
+                baseIndent: 1,
+                switchCase: 1
+            }
+        ]
+    }
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+    pages: {
+        app: {
+            entry: 'src/main.js',
+            excludeChunks: ['silent-renew-oidc'],
+            filename: 'index.html',
+            template: 'public/index.html'
+        },
+        silentRenewOidc: {
+            entry: 'src/silent-renew-oidc.js',
+            excludeChunks: ['app'],
+            filename: 'silent-renew-oidc.html',
+            template: 'public/silent-renew-oidc.html'
+        }
+    },
+    transpileDependencies: ['vuetify'],
+    lintOnSave: false
+});
